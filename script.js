f07d84448e33313035c447a4ffbc610bf09a5ecb
Kamyar Khoshkalam <kamyarkhoshkalam@gmail.com>
2025-07-06 23:16:40 +0330

    add local storage

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const main = document.querySelector("main");
 const subButton = form.querySelector("#sub-button");
 const items = document.querySelector(".items");
 
+let itemsList = [];
+
 addItem.addEventListener("click", () => {
   form.classList.toggle("hide");
 });
@@ -17,9 +19,12 @@ document.querySelector("body").addEventListener("click", (e) => {
   }
 });
 
-const createItem = function (title, description) {
+const createItem = function (title, description, donestatus) {
   const div = document.createElement("div");
   div.classList.add("item");
+  if (donestatus) {
+    div.classList.add("done-task");
+  }
   const itemTitle = document.createElement("h3");
   itemTitle.innerText = title;
   const itemDescription = document.createElement("p");
@@ -30,13 +35,15 @@ const createItem = function (title, description) {
   done.classList.add("btn-done");
   done.innerText = "done";
   done.addEventListener("click", () => {
-    if (div.style.textDecoration == "line-through") {
-      div.style.textDecoration = "";
-      done.innerText = "done";
-    } else {
-      div.style.textDecoration = "line-through";
-      done.innerText = "undone";
-    }
+    div.classList.toggle("done-task");
+
+    itemsList.forEach((item) => {
+      if (item.title === title && item.description === description) {
+        item.done = !item.done;
+      }
+    });
+
+    localStorage.setItem("todo-items", JSON.stringify(itemsList));
   });
   const edit = document.createElement("button");
   edit.classList.add("btn-edit");
@@ -45,6 +52,12 @@ const createItem = function (title, description) {
   deletetask.classList.add("btn-delete");
   deletetask.innerText = "delete";
   deletetask.addEventListener("click", (e) => {
+    itemsList = itemsList.filter((element) => {
+      return (
+        element.title != e.target.closest(".item").querySelector("h3").innerText
+      );
+    });
+    localStorage.setItem("todo-items", JSON.stringify(itemsList));
     e.target.closest(".item").remove();
   });
   btnDiv.append(done);
@@ -55,14 +68,31 @@ const createItem = function (title, description) {
   div.append(btnDiv);
   items.insertAdjacentElement("afterbegin", div);
 };
+const save = localStorage.getItem("todo-items");
+if (save) {
+  itemsList = JSON.parse(save);
+  itemsList.forEach((items) =>
+    createItem(items.title, items.description, items.done)
+  );
+}
+
+const showItem = function () {
+  itemsList.forEach((element) => {
+    createItem(element.title, element.description);
+  });
+};
+
 subButton.addEventListener("click", (e) => {
   e.preventDefault();
   const titleVal = document.querySelector("#title-add").value.trim();
+  const descriptionVal = document.querySelector("#description").value.trim();
   if (!titleVal) {
     alert("please enter some value");
     return;
   }
+  itemsList.push({ title: titleVal, description: descriptionVal, done: false });
+  localStorage.setItem("todo-items", JSON.stringify(itemsList));
+  createItem(titleVal, descriptionVal, false);
+  document.querySelector("#title-add").value = "";
+  document.querySelector("#description").value = "";
 });
-
-createItem("ali", "ali task");
-createItem("akbar", "akbar task");
